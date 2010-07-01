22d48a7ba076ba2e97adc6cc788c3de9a093413e
Torsten Fehre <post@feson.de>
2010-07-01 17:03:24 +0200

    Deleted unneded code

--- sync_from_server.pl
+++ sync_from_server.pl
@@ -19,46 +19,6 @@ my $after_action = $ARGV[0];
 if ($after_action ne '' && $after_action ne 'mail' && $after_action ne 'summary') {
 	die("Usage: sync_from_server.pl [mail|summary]");
 }
-my $sthGetArea = Db::dbh->prepare(q(
-		SELECT * FROM t_areas WHERE id=?
-	));
-my $sthInsertArea = Db::dbh->prepare(q(
-		INSERT INTO t_areas(id, name) VALUES (?, ?)
-	));
-my $sthGetIssue = Db::dbh->prepare(q(
-	SELECT * FROM t_issues WHERE id=?
-));
-my $sthUpdateIssue = Db::dbh->prepare(q(
-	UPDATE t_issues SET
-		state=?,
-		created_at=?,
-		accepted_at = ?,
-		half_frozen_at = ?,
-		fully_frozen_at = ?,
-		closed_at = ?
-	WHERE id=?
-));
-my $sthInsertIssue = Db::dbh->prepare('
-	INSERT INTO t_issues(id, area_id, state, created_at, accepted_at, half_frozen_at, fully_frozen_at, closed_at)
-	VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-');
-my $sthGetInitiative = Db::dbh->prepare('
-	SELECT * FROM t_initiatives WHERE id=?
-');
-my $sthUpdateInitiative = Db::dbh->prepare('
-	UPDATE t_initiatives SET
-		name = ?,
-		discussion_url = ?,
-		created_at = ?,
-		draft_updated_at = ?,
-		draft_content = ?
-	WHERE id = ?
-');
-my $sthInsertInitiative = Db::dbh->prepare('
-	INSERT INTO t_initiatives(
-		id, issue_id, name, discussion_url, created_at, draft_updated_at, draft_content)
-		VALUES (?, ?, ?, ?, ?, ?, ?)
-');
 
 my @all_initiatives = $xml_doc->getElement('initiative');
 my $updates = new CUpdateCollection;
